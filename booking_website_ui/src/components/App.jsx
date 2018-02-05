9971164edb34ea49b889e1c9109db9d9f069e66a
RyanMoss96 <rpmoss96@gmail.com>
2018-02-05 19:23:04 +0000

    Paper

--- booking_website_ui/src/components/App.jsx
+++ booking_website_ui/src/components/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Switch, Route } from 'react-router-dom'
-import Test from './Test';
+import Home from './Home';
 import Header from './Header';
 
 const App = () => (
     <MuiThemeProvider>
         <Header />
         <Switch>
-            <Route exact path='/' component={Test} />
+            <Route exact path='/' component={Home} />
         </Switch>
     </MuiThemeProvider>
 )
--- booking_website_ui/src/components/Header.jsx
+++ booking_website_ui/src/components/Header.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography'
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
 
-export default class Header extends React.Component {
-    constructor() {
-        super();
-    }
+const styles = {
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+}
 
+class Header extends React.Component {
     render() {
+        const {classes} = this.props;
         return (
             <div>
                 <AppBar position="static">
                     <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
                         <Typography variant="title" color="inherit">
                             Appointment Bookings
                         </Typography>
@@ -22,3 +32,5 @@ export default class Header extends React.Component {
         );
     }
 }
+
+export default withStyles(styles)(Header);
